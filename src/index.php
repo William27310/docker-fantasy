6a9459f850509993457d650aece644e5176a8856
William27310 <william.27@free.fr>
2025-08-27 17:29:33 +0200

    avancée2

--- src/index.php
+++ src/index.php
@@ -3,8 +3,33 @@
 require "Character.php";
 require "Guerrier.php";
 require "Orc.php";
+session_start();
 
+var_dump($_POST);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   if (isset($_POST['action'])) {
+      switch ($_POST['action']) {
+         case 'create-warrior':
+            $barbare = new Guerrier(2000, 1000, "Sun's blade", 250, "Sun's Shield", 600);
+            $_SESSION['barbare'] = $barbare;
+            break;
+
+         case 'create-orc':
+            $orque = new Orc(1500, 200, 150, 300);
+            $_SESSION['orque'] = $orque;
+            break;
+
+         case 'begin':
+            echo "Le dé se lance..";
+            break;
+
+         case 'fight':
+            echo "Le combat va commencer..";
+            break;
+      }
+   }
+}
 
 
 ?>
@@ -28,58 +53,68 @@ require "Orc.php";
    <div>
       <div class="d-flex justify-content-around mt-5 mb-5">
 
-         <div class="card" style="width: 18rem;">
-            <img src="assets/img/bryan.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-               <h4 class="mt-4"> Guerrier </h4>
-               <hr>
-               <p class="card-text"><span> Health : </span> 500 <br><span> Mana : </span> 500 <br></p>
-               <p><span> Weapon : </span> 500 <br><span> Weapon Damage : </span>500 <br></p>
-               <p class="card-text"><span> Shield : </span>500 <br><span> Shield Absorption : </span>500 <br></p>
+         <?php if (isset($_SESSION['barbare'])) { ?>
+
+            <div class="card" style="width: 18rem;">
+               <img src="assets/img/bryan.jpg" class="card-img-top" alt="...">
+               <div class="card-body">
+                  <h4 class="mt-4"> Guerrier </h4>
+                  <hr>
+                  <p class="card-text"><span> Health : </span> <?= $_SESSION['barbare']->getHealth() ?> <br><span> Mana : </span> <?= $_SESSION['barbare']->getMana() ?> <br></p>
+                  <p><span> Weapon : </span> <?= $_SESSION['barbare']->getWeapon() ?> <br><span> Weapon Damage : </span><?= $_SESSION['barbare']->getWeaponDamage() ?> <br></p>
+                  <p class="card-text"><span> Shield : </span><?= $_SESSION['barbare']->getShield() ?> <br><span> Shield Absorption : </span><?= $_SESSION['barbare']->getShieldValue() ?> <br></p>
+               </div>
             </div>
-         </div>
+
+         <?php } ?>
 
          <h1>VS</h1>
 
-         <div class="card-test">
-            <div class="card" style="width: 18rem;">
-               <img src="assets/img/Orc.jpg" class="card-img-top" alt="...">
-               <div class="card-body">
-                  <h4 class="mt-4"> Orc </h4>
-                  <hr>
-                  <p class="card-text"><span>Health : </span> 500 <br><span> Mana : </span> 500 <br></p>
-                  <p><span> Damage Min : </span> 500 <br><span> Damage Max : </span> 500 <br></p>
+         <?php if (isset($_SESSION['orque'])) { ?>
+
+            <div class="card-test">
+               <div class="card" style="width: 18rem;">
+                  <img src="assets/img/Orc.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h4 class="mt-4"> Orc </h4>
+                     <hr>
+                     <p class="card-text"><span>Health : </span> <?= $_SESSION['orque']->gethealth() ?> <br><span> Mana : </span> <?= $_SESSION['orque']->getMana() ?> <br></p>
+                     <p><span> Damage Min : </span> <?= $_SESSION['orque']->getDamageMin() ?> <br><span> Damage Max : </span> <?= $_SESSION['orque']->getDamageMax() ?> <br></p>
+                  </div>
                </div>
             </div>
-         </div>
       </div>
 
+   <?php } ?>
+
+   <div class="formulaire d-flex justify-content-around">
+      <form action="" method="POST">
+         <input type="hidden" name="action" value="create-warrior">
+         <button class="btn btn-dark p-4"> Créer le guerrier </button>
+      </form>
+
+      <form action="" method="post">
+         <input type="hidden" name="action" value="create-orc">
+         <button class="btn btn-dark p-4"> Créer l'orc </button>
+      </form>
+
+      <form action="" method="post">
+         <input type="hidden" name="action" value="begin">
+         <button class="btn btn-dark p-4"> Qui commence ? </button>
+      </form>
+
+      <form action="" method="post">
+         <input type="hidden" name="action" value="fight">
+         <button class="btn btn-dark p-4"> Combat ! </button>
+      </form>
+   </div>
+
+   <div class="d-flex justify-content-center mt-4 mb-5">
+      <a class="btn btn-dark" href="reset.php">Effacer les données</a>
+   </div>
 
 
-      <div class="formulaire d-flex justify-content-around">
-         <form action="" method="POST">
-            <input type="hidden" name="action" value="create-warrior">
-            <button class="btn btn-dark p-4"> Créer le guerrier </button>
-         </form>
-
-         <form action="" method="post">
-            <input type="hidden" name="action" value="create-orc">
-            <button class="btn btn-dark p-4"> Créer l'orc </button>
-         </form>
-
-         <form action="" method="post">
-            <input type="hidden" name="action" value="begin">
-            <button class="btn btn-dark p-4"> Qui commence ? </button>
-         </form>
-
-         <form action="" method="post">
-            <input type="hidden" name="action" value="fight">
-            <button class="btn btn-dark p-4"> Combat ! </button>
-         </form>
-      </div>
-
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
 </html>
